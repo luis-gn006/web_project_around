9bd11564c75ec9dd7387d3036d418b953fa8805f
LuisOn117 <luis-enrique06@outlook.com>
2024-06-25 19:48:25 -0600

    program load cards

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,58 @@
-let popup = document.querySelector(".popup");
+let popupProfile = document.querySelector(".popup__profile");
 let editButton = document.querySelector(".profile__button-edit");
-let closeButton = document.querySelector(".popup__close-button");
+let popupElements = document.querySelector(".popup__elements");
+let addButton = document.querySelector(".profile__button-add");
+let closeButtonProfile = popupProfile.querySelector(".popup__close-button");
+let closeButtonElements = popupElements.querySelector(".popup__close-button");
 
 let profileName = document.querySelector(".profile__name");
 let profileJob = document.querySelector(".profile__job");
 let nameForm = document.querySelector(".popup__form-name");
 let jobForm = document.querySelector(".popup__form-job");
 
-/* Funcion visulacion popup */
+let elementsArea = document.querySelector(".elements");
+let templateNode = document.querySelector(".element__template");
+
+const initialCards = [
+  {
+    name: "Disney , USA",
+    link: "https://images.unsplash.com/photo-1605443791607-80a259dd3c3c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Eiffel , Francia",
+    link: "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Fuente de Trevi , Italia",
+    link: "https://images.unsplash.com/photo-1596627116790-af6f46dddbda?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Puebla , México",
+    link: "https://images.unsplash.com/photo-1666918057592-b9b9c98fb952?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Lazio , Roma",
+    link: "https://images.unsplash.com/photo-1614119189147-c55075cba781?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Yucatán , México",
+    link: "https://plus.unsplash.com/premium_photo-1676517308970-99f81614f4f2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
+/* Funcion visulacion popup profile*/
 function editButtonClick() {
-  popup.classList.toggle("popup__opened");
+  popupProfile.classList.toggle("popup__opened");
 }
 editButton.addEventListener("click", editButtonClick);
-closeButton.addEventListener("click", editButtonClick);
+closeButtonProfile.addEventListener("click", editButtonClick);
+
+/* Funcion visulacion popup elements*/
+function addButtonClick() {
+  popupElements.classList.toggle("popup__opened");
+}
+addButton.addEventListener("click", addButtonClick);
+closeButtonElements.addEventListener("click", addButtonClick);
 
 /* Llamar info del perfil*/
 function callProfileInfo() {
@@ -22,32 +62,25 @@ function callProfileInfo() {
 
 editButton.addEventListener("click", callProfileInfo);
 
-// Busquemos el formulario en el DOM
 let elementForm = document.querySelector(".popup__form");
 let savePopupButton = document.querySelector(".popup__form-button");
-// Lo siguiente es el manipulador (handler) de entrega de formularios, aunque
-// no se enviará en ningún sitio todavía
 
-// Observa que el nombre de la función comienza con un verbo
-// y describe exactamente lo que hace la función
 function handleProfileFormSubmit(evt) {
-  // Esta línea impide que el navegador
-  // entregue el formulario en su forma predeterminada.
   evt.preventDefault();
-  // Una vez hecho esto, podemos definir nuestra propia forma de entregar el formulario.
-  // Lo explicaremos todo con más detalle después.
-
-  // Busquemos los campos del formulario en el DOM
-
-  // Obtén los valores de cada campo desde la propiedad de valor correspondiente
-
-  // Selecciona los elementos donde se introducirán los valores de los campos
-
   profileName.textContent = nameForm.value;
   profileJob.textContent = jobForm.value;
-  popup.classList.toggle("popup__opened");
-  // propiedad del método querySelector()
+  popupProfile.classList.toggle("popup__opened");
 }
 
 savePopupButton.addEventListener("click", handleProfileFormSubmit);
-// se observará el evento de entrega
+
+/* Funcion cargar imagenes de inicio*/
+initialCards.forEach(function (item) {
+  const newNode = templateNode.content
+    .querySelector(".element")
+    .cloneNode(true);
+  newNode.querySelector(".element__image").src = item.link;
+  newNode.querySelector(".element__image").alt = `imagen de ${item.name} `;
+  newNode.querySelector(".element__name").textContent = item.name;
+  elementsArea.append(newNode);
+});
