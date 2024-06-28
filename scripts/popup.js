93ac0215d4098db75623930b1ee36c865dbc124f
LuisOn117 <luis-enrique06@outlook.com>
2024-06-27 18:26:50 -0600

    program image popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,3 @@
-let popupProfile = document.querySelector(".popup__profile");
-let editButton = document.querySelector(".profile__button-edit");
-let popupElements = document.querySelector(".popup__elements");
-let addButton = document.querySelector(".profile__button-add");
-let closeButtonProfile = popupProfile.querySelector(".popup__close-button");
-let closeButtonElements = popupElements.querySelector(".popup__close-button");
-
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
-let nameForm = document.querySelector(".popup__form-name");
-let jobForm = document.querySelector(".popup__form-job");
-
-let elementsArea = document.querySelector(".elements");
-let templateNode = document.querySelector(".element__template");
-
 const initialCards = [
   {
     name: "Disney , USA",
@@ -41,6 +26,9 @@ const initialCards = [
 ];
 
 /* Funcion visulacion popup profile*/
+let popupProfile = document.querySelector(".popup__profile");
+let editButton = document.querySelector(".profile__button-edit");
+let closeButtonProfile = popupProfile.querySelector(".popup__close-button");
 function editButtonClick() {
   popupProfile.classList.toggle("popup__opened");
 }
@@ -48,6 +36,9 @@ editButton.addEventListener("click", editButtonClick);
 closeButtonProfile.addEventListener("click", editButtonClick);
 
 /* Funcion visulacion popup elements*/
+let popupElements = document.querySelector(".popup__elements");
+let addButton = document.querySelector(".profile__button-add");
+let closeButtonElements = popupElements.querySelector(".popup__close-button");
 function addButtonClick() {
   popupElements.classList.toggle("popup__opened");
 }
@@ -55,6 +46,10 @@ addButton.addEventListener("click", addButtonClick);
 closeButtonElements.addEventListener("click", addButtonClick);
 
 /* Llamar info del perfil*/
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let nameForm = document.querySelector(".popup__form-name");
+let jobForm = document.querySelector(".popup__form-job");
 function callProfileInfo() {
   nameForm.value = profileName.textContent;
   jobForm.value = profileJob.textContent;
@@ -77,6 +72,9 @@ function handleProfileFormSubmit(evt) {
 savePopupButton.addEventListener("click", handleProfileFormSubmit);
 
 /* Funcion cargar imagenes de inicio*/
+let elementsArea = document.querySelector(".elements");
+let templateNode = document.querySelector(".element__template");
+
 initialCards.forEach(function (item) {
   let newNode = createCard(item.name, item.link);
   elementsArea.append(newNode);
@@ -110,11 +108,29 @@ function createCard(name, link) {
     .addEventListener("click", function (evt) {
       evt.target.classList.toggle("element__button-like-active");
     });
-
+  /*funcion borrar tarjeta*/
   newNode
     .querySelector(".element__button-trash")
     .addEventListener("click", function (evt) {
       newNode.remove();
     });
+  /*funcion abrir popup image*/
+  newNode.querySelector(".element__image").addEventListener("click", () => {
+    let popupImage = document.querySelector(".popup__image-fullscreen");
+    popupImage.classList.toggle("popup__opened");
+    /*funcion llamar info al popup*/
+    popupImage.querySelector(".popup__image").src = link;
+    popupImage.querySelector(".popup__image").alt = `imagen de ${name} `;
+    popupImage.querySelector(".popup__title-image").textContent = name;
+  });
+
   return newNode;
 }
+
+/* funcion cerrar popup image*/
+let closeButtonImage = document.querySelector(".popup__button-image");
+function closeButtonClick() {
+  let popupImage = document.querySelector(".popup__image-fullscreen");
+  popupImage.classList.toggle("popup__opened");
+}
+closeButtonImage.addEventListener("click", closeButtonClick);
