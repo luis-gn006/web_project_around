4491d49a1a1ab1a4c119571cc0cb7d0d66579ac4
lapLGH <luis-enrique06@outlook.com>
2024-07-16 18:38:18 -0600

    complete validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,23 +1,33 @@
-/*función validar formularios*/
-const showInputError = (formElement, inputElement, errorMessage) => {
+/*function validar formularios*/
+const showInputError = (
+  formElement,
+  inputElement,
+  errorMessage,
+  formConfig
+) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.nextElementSibling.classList.add("popup__form-input_error");
+  inputElement.nextElementSibling.classList.add(formConfig.inputErrorClass);
   errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup__input-error_active");
+  errorElement.classList.add(formConfig.errorClass);
 };
 
-const hideInputError = (formElement, inputElement) => {
+const hideInputError = (formElement, inputElement, formConfig) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.nextElementSibling.classList.remove("popup__form-input_error");
-  errorElement.classList.remove("popup__input-error_active");
+  inputElement.nextElementSibling.classList.remove(formConfig.inputErrorClass);
+  errorElement.classList.remove(formConfig.errorClass);
   errorElement.textContent = "";
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, formConfig) => {
   if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      formConfig
+    );
   } else {
-    hideInputError(formElement, inputElement);
+    hideInputError(formElement, inputElement, formConfig);
   }
 };
 
@@ -29,49 +39,53 @@ const hasInvalidInput = (inputList) => {
 
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("popup__form-button_disabled");
+    buttonElement.disabled = true;
   } else {
-    buttonElement.classList.remove("popup__form-button_disabled");
+    buttonElement.disabled = false;
   }
 };
 
-const setEventListeners = (formElement) => {
+const setEventListeners = (formElement, formConfig) => {
   const inputList = Array.from(
-    formElement.querySelectorAll(".popup__form-input")
+    formElement.querySelectorAll(formConfig.inputSelector)
   );
-  const buttonElement = formElement.querySelector(".popup__form-button");
+  const buttonElement = formElement.querySelector(
+    formConfig.submitButtonSelector
+  );
   toggleButtonState(inputList, buttonElement);
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
-      checkInputValidity(formElement, inputElement);
+      checkInputValidity(formElement, inputElement, formConfig);
       toggleButtonState(inputList, buttonElement);
     });
   });
 };
 
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll(".popup__form"));
+const enableValidation = (formConfig) => {
+  const formList = Array.from(
+    document.querySelectorAll(formConfig.formSelector)
+  );
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
     });
 
     const fieldsetList = Array.from(
-      formElement.querySelectorAll(".popup__form-set")
+      formElement.querySelectorAll(formConfig.fieldsetSelector)
     );
 
     fieldsetList.forEach((fieldset) => {
-      setEventListeners(fieldset);
+      setEventListeners(fieldset, formConfig);
     });
   });
 };
 
 enableValidation({
-  formSelector: "popup__form",
-  inputSelector: "popup__form-input",
-  submitButtonSelector: "popup__form-button",
+  formSelector: ".popup__form",
+  inputSelector: ".popup__form-input",
+  submitButtonSelector: ".popup__form-button",
   inactiveButtonClass: "popup__form-button_disabled",
   inputErrorClass: "popup__form-input_error",
   errorClass: "popup__input-error_active",
-  fieldsetSelector: "popup__form-set",
+  fieldsetSelector: ".popup__form-set",
 });
